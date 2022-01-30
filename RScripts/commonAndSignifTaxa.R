49fed2cb802119ae373f1edd2321e72469755d55
anhmoss <anh.moss@gmail.com>
2022-01-30 14:20:51 -0500

    Update and rename 00_commonAndSignifTaxa_jan12.R to commonAndSignifTaxa.R

--- RScripts/commonAndSignifTaxa.R
+++ RScripts/commonAndSignifTaxa.R
@@ -1,4 +1,49 @@
 
+#Step 1: reads in counts table, labeled by cohort name
+  #input: one file input (filepaths and cohort names as columns)
+  #output: list of OTU counts table for all 11 datasets
+
+raw_q2021_filespaths = read.table(myFilePath, header = TRUE, sep = "\t")
+raw_q2021_files = list()
+
+for(i in 1:nrow(raw_q2021_filespaths)) {
+  raw_q2021_files[[i]] = read.table(raw_q2021_filespaths[i,1], header = TRUE, sep = "\t")
+}
+
+names(raw_q2021_files) = raw_q2021_filespaths[,2]
+
+
+#Step 2: lognormalize counts for each dataset, perform stats test (taxa ~ age), permanova test, alpha diversity analysis at the genus level
+  #input: counts table for each dataset (ie each element from the list)
+  #output: stats calculations (non-parametric and parametric), permanova results per cohort, alpha diversity results and plots (richness and shannon diversity)
+  
+stats_fdrs_list = list()
+lm_results_list = list()
+coh_l = vector()
+
+
+for(i in 1:11){
+  raw_file = raw_q2021_files[[i]]
+  taxa_table = raw_file[,grep("g__",colnames(raw_file))]
+  taxa_table = taxa_table[rowSums(taxa_table)!=0,]
+  age_meta = raw_file$Age[rowSums(taxa_table)!=0]
+  
+  lognorm_file = lognorm_function(taxa_table)
+  names_short=sapply(strsplit(colnames(lognorm_file),"g__"),"[[",2)
+  colnames(lognorm_file)=names_short
+  lognorm_file=lognorm_file[,colSums(lognorm_file)!=0]
+
+  coh_l[i]=raw_file[1,1]
+  
+  stat_output = stat_correlation_function(counts=lognorm_file, age = age_meta, corrMethod = "kendall")
+  stats_fdrs_list[[i]] = stat_output
+  
+  lm_output = stat_simpleLM_function(counts=lognorm_file, age = age_meta)
+  lm_results_list[[i]] = lm_output
+
+}
+
+## Step 2: Check for taxa that are common across all datasets, then check for the significant (FDR 5%) among the common taxa
 
 list1 = rownames(stats_fdrs_list[[1]])
 list2 = rownames(stats_fdrs_list[[2]])
@@ -16,19 +61,17 @@ all_list = list(list1, list2, list3, list4, list5, list6,
                 list7, list8, list9, list10, list11)
 
 reduced_list = Reduce(intersect, all_list) #86 total
-reduced_list_narrow = reduced_list[-(grep("UCG|uncultured", reduced_list))] #70 total
+reduced_list_narrow = reduced_list[-(grep("UCG|uncultured", reduced_list))] #70 total, exclues genera that are uncultured or unclassified
 
-
-testmerge_lists = list()
-
+#pulls stats results for only taxa that are common in all 11 cohorts (reduce_list_narrow)
 commonGenus_narrow_list = list()
 
 for(i in 1:11){
   commonGenus_narrow_list[[i]] = stats_fdrs_list[[i]][rownames(stats_fdrs_list[[i]]) %in% reduced_list_narrow,]
 }
 
-commonGenus_narrow_list[[1]][(commonGenus_narrow_list[[1]][,3] < 0.05),,drop=F]
 
+#among the common taxa, pull the ones that have fdr adjusted pvals  < 0.05 for each cohort
 commonAndSignificant_list = list()
 names(commonAndSignificant_list) = coh_l
 
@@ -81,23 +124,5 @@ sum(cs_taxa_agp %in% cs_taxa_nogbcn0) # 3 matches: "Bacteroides" "Howardella"  "
 "Bifidobacterium" %in% cs_taxa_nogbcn0
 
 
-countsMatched = NULL
-cs_taxa_matched_lists = list()
 
-for(i in 1:5){
-  for(j in (i+1):6){
-  countsMatched[i] = sum(cs_taxa_allList[[i]] %in% cs_taxa_allList[[j]])
-  cs_taxa_matched_lists[[j]] =cs_taxa_allList[[i]][cs_taxa_allList[[i]] %in% cs_taxa_allList[[j]]]
-  }
-}
 
-for(i in 1:5){
-  for(k in (i+1):6){
-  # for(j in 1:length(cs_taxa_matched_lists[[i]])){
-    if(cs_taxa_allList[[i]] %in% cs_taxa_allList[[j]]){
-      
-    }
-  }
-}
-
-length(cs_taxa_allList[[5]])

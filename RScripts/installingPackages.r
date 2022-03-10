c2d3fb925559bdd9a40433c4d8c695b86e98661d
anhmoss <anh.moss@gmail.com>
2022-03-10 00:01:33 -0500

    Create installingPackages.r

---
+++ RScripts/installingPackages.r
@@ -0,0 +1,5 @@
+# Installing and loading packages for this study
+
+packagesToInstall = c("ggplot2", "ggpubr", "vegan", "dplyr")
+install.packages(packagesToInstall) 
+lapply(packagesToInstall, library, character.only=TRUE)

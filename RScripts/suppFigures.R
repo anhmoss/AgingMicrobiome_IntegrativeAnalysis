8e8e6c73c14d35ace06c3ed137a20eb3c6b647bd
anhmoss <anh.moss@gmail.com>
2022-03-16 01:45:58 -0400

    Update suppFigures.R

--- RScripts/suppFigures.R
+++ RScripts/suppFigures.R
@@ -1,4 +1,4 @@
-generatePlots = function(myFilePath, resultDirPath) {
+generateSuppFigures = function(myFilePath, resultDirPath) {
   
   #Step 1: reads in counts table, labeled by cohort name
   #input: one file input (filepaths and cohort names as columns)
@@ -15,7 +15,7 @@ generatePlots = function(myFilePath, resultDirPath) {
   #extracts only stool samples from Morgan cohort for subsequent analysis
   raw_q2021_files[[1]] = subset(raw_q2021_files[[1]], raw_q2021_files[[1]]$stool==1)
   
-  
+  pdf(paste0(resultDirPath, "AgeRangeDist_Boxplot.pdf"),onefile = T)
   #plot age range distribution boxplots
   boxplot(
   raw_q2021_files$NogBCN0$Age,
@@ -49,7 +49,40 @@ allstudies_agerange_list <- list(
   "Morgan" = raw_q2021_files$Morgan$Age,
   "AGP"=raw_q2021_files$AGP$Age)
 stripchart(allstudies_agerange_list, method = "jitter", pch = 20, vertical= TRUE, add = TRUE)
+dev.off()
+  
+# shannon div comparison on raw vs rarefied counts ______________________________________________________________________________________________
+raw_merge_allCounts = full_join(raw_q2021_files[[1]], raw_q2021_files[[2]])
 
+for(i in 3:11){ 
+  file = raw_q2021_files[[i]]
+  raw_merge_allCounts = full_join(raw_merge_allCounts, file)
+}
+
+raw_merge_allCounts[is.na(raw_merge_allCounts)] = 0
+
+raw_merge_allCounts_genusTaxa = raw_merge_allCounts[,grep("g__", colnames(raw_merge_allCounts))]
+  
+set.seed(316)
+#all pooled samples, raw (not normalized), not rarefied
+allGenera_raw = raw_merge_allCounts_genusTaxa
+allGenera_raw_shannonDiv = diversity(allGenera_raw,index = "shannon", MARGIN = 1, base = exp(1))
+
+#shannon div on rarefied counts
+allGenera_raw_rarefied= rrarefy(allGenera_raw, 1000)
+allGenera_raw_rarefied_shannonDiv = diversity(allGenera_raw_rarefied,index = "shannon", MARGIN = 1, base = exp(1))
+allGenera_raw_rarefied_shannonDiv[which(rowSums(allGenera_raw_rarefied)<1000)] = NA
+
+pdf(paste0(resultDirPath, "ShannonDiversity_RawVSRarefiedCounts.pdf"),onefile = T)
+plot(allGenera_raw_shannonDiv,
+     allGenera_raw_rarefied_shannonDiv,
+     main = "Shannon Diversity on Raw vs Rarefied Counts",
+     xlab = "Shannon Diversity (Raw Counts)",
+     ylab = "Shannon Diversity (Rarefied Counts)")
+ dev.off()
+ 
+  
+ #____________________________________________________________________________________________________________________________________________________
 
   #Step 2: lognormalize counts for each dataset, perform stats test 
   #input: counts table for each dataset (ie each element from the list)
